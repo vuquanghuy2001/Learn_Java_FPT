c26bb16f441e10248c440d7601f95c07d5534c2c
Vũ Quang Huy <vuquanghuyxl1234@gmail.com>
2021-07-04 14:59:17 +0700

    update

---
+++ hellospringsecurity/src/main/java/com/example/hellospringsecurity/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.hellospringsecurity;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable().authorizeRequests()
+                .antMatchers("/users").hasRole("admin")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll();
+    }
+}
